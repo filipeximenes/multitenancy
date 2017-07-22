34131b3c8974c2f741a9124bb400be70e27ab432
Filipe A Ximenes <filipeximenes@gmail.com>
2017-07-22 11:29:15 -0300

    formating

--- scripts/queries.py
+++ scripts/queries.py
@@ -9,25 +9,28 @@ request = Object()
 request.customer = customer
 
 # get the avarage spin duration duration
-avg_duration = (Spin.objects
-                .filter(user_spinner__user__customer=request.customer)
-                .aggregate(avg=Avg('duration')))['avg']
+avg_duration = (
+    Spin.objects
+    .filter(user_spinner__user__customer=request.customer)
+    .aggregate(avg=Avg('duration')))['avg']
 
 print('Average duration:', avg_duration, 's')
 
-avg_duration = (Spin.objects
-                .filter(customer=request.customer)
-                .aggregate(avg=Avg('duration')))['avg']
+avg_duration = (
+    Spin.objects
+    .filter(customer=request.customer)
+    .aggregate(avg=Avg('duration')))['avg']
 
 print('Average duration:', avg_duration, 's')
 
 
 # get a list of spinners ordered from the fastest to the slowest
-spinners = (Spinner.objects
-            .filter(customer=request.customer)
-            .annotate(
-                avg_duration=Avg('owned_spinners__spins__duration'))
-            .order_by('-avg_duration'))
+spinners = (
+    Spinner.objects
+    .filter(customer=request.customer)
+    .annotate(
+        avg_duration=Avg('owned_spinners__spins__duration'))
+    .order_by('-avg_duration'))
 
 print('Average duration for spinner:')
 for spinner in spinners:
