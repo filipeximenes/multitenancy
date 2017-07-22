adcf47c60eef83bf3eede42a58d9a92dc46f57b6
Filipe A Ximenes <filipeximenes@gmail.com>
2017-07-22 11:26:55 -0300

    uses 'request' obj in examples

--- scripts/queries.py
+++ scripts/queries.py
@@ -5,15 +5,18 @@ from customers.models import Customer
 
 customer = Customer.objects.first()
 
+request = Object()
+request.customer = customer
+
 # get the avarage spin duration duration
 avg_duration = (Spin.objects
-                .filter(user_spinner__user__customer=customer)
+                .filter(user_spinner__user__customer=request.customer)
                 .aggregate(avg=Avg('duration')))['avg']
 
 print('Average duration:', avg_duration, 's')
 
 avg_duration = (Spin.objects
-                .filter(customer=customer)
+                .filter(customer=request.customer)
                 .aggregate(avg=Avg('duration')))['avg']
 
 print('Average duration:', avg_duration, 's')
@@ -21,7 +24,7 @@ print('Average duration:', avg_duration, 's')
 
 # get a list of spinners ordered from the fastest to the slowest
 spinners = (Spinner.objects
-            .filter(customer=customer)
+            .filter(customer=request.customer)
             .annotate(
                 avg_duration=Avg('owned_spinners__spins__duration'))
             .order_by('-avg_duration'))
